971949436403c36518f50005e855ade6c5d3f7f3
Joshua Chua <jc5157@truman.edu>
2014-11-03 20:47:48 -0600

    edit school model, more translations, and twitter

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,4 +1,4 @@
-class State < ActiveRecord::Base
+class School < ActiveRecord::Base
   validates_presence_of :name, :acronym
   validates_uniqueness_of :name, :acronym
   has_many :user
@@ -7,7 +7,7 @@ class State < ActiveRecord::Base
   belongs_to :state
   def self.array
     return @array if @array
-    @array = [ "Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "District of Columbia", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming" ]
+    @array = []
     self.order(:name).each do |state|
       @array << [state.name, state.acronym]
     end
