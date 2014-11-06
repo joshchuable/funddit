ad09e2982e48d5a1cdbaa0554bab865b64524609
Josh <jc5157@truman.edu>
2014-11-05 20:03:19 -0500

    what what

--- config/initializers/sendgrid.rb
+++ config/initializers/sendgrid.rb
@@ -6,7 +6,7 @@ begin
       authentication: :plain,
       user_name: CatarseSettings.get_without_cache(:sendgrid_user_name),
       password: CatarseSettings.get_without_cache(:sendgrid),
-      domain: 'heroku.com'
+      domain: 'funddit.me'
     }
     ActionMailer::Base.delivery_method = :smtp
   end
